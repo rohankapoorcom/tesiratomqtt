c66f3d71a464b79be354f6fa52925ad064d478b5
Rohan Kapoor <rohan@rohankapoor.com>
2025-02-25 00:32:03 -0800

    Switch to utf-8 encoding

--- src/telnet.py
+++ src/telnet.py
@@ -78,14 +78,14 @@ class BiampTesiraTelnetConnection:
         # Read the response, with optional timeout
         try:
             data = await asyncio.wait_for(
-                self.reader.readuntil(separator.encode("ASCII")), timeout_time
+                self.reader.readuntil(separator.encode("utf-8")), timeout_time
             )
         except TimeoutError as err:
             raise ClientTimeoutError from err
         except (OSError, asyncio.IncompleteReadError) as err:
             raise ClientConnectionError from err
 
-        return data.replace(b"\x00", b"").decode("ASCII")
+        return data.replace(b"\x00", b"").decode("utf-8")
 
     async def readline(self, timeout_time: float | None = None) -> str:
         """
